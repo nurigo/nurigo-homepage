c2d68afde2c9933155ac27f5b7ee8a9e694cf0db
wileyday <wiley@nurigo.net>
2017-09-04 11:07:35 +0900

    IP, 로드맵 추가

--- themes/hugo-elate-theme/static/js/main.js
+++ themes/hugo-elate-theme/static/js/main.js
@@ -322,6 +322,49 @@
 		}
 	};
 
+	var roadmapAnimate = function() {
+		var services = $('#fh5co-roadmap');
+		if ( services.length > 0 ) {	
+
+			services.waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+					var sec = services.find('.to-animate').length,
+						sec = parseInt((sec * 200) + 400);
+
+					setTimeout(function() {
+						services.find('.to-animate').each(function( k ) {
+							var el = $(this);
+							
+							setTimeout ( function () {
+								el.addClass('fadeInUp animated');
+							},  k * 200, 'easeInOutExpo' );
+							
+						});
+					}, 200);
+
+					setTimeout(function() {
+						services.find('.to-animate-2').each(function( k ) {
+							var el = $(this);
+							
+							setTimeout ( function () {
+								el.addClass('bounceIn animated');
+							},  k * 200, 'easeInOutExpo' );
+							
+						});
+					}, sec);
+
+
+					
+					$(this.element).addClass('animated');
+						
+				}
+			} , { offset: '80%' } );
+
+		}
+	};
+
 	var aboutAnimate = function() {
 		var about = $('#fh5co-about');
 		if ( about.length > 0 ) {	
@@ -463,6 +506,7 @@
 		workAnimate();
 		testimonialAnimate();
 		servicesAnimate();
+		roadmapAnimate();
 		aboutAnimate();
 		countersAnimate();
 		contactAnimate();
